e47839a73a45dcdbce9d9ffaad9e7556bd4090aa
Jimeto <jp.onyeabo@gmail.com>
2025-11-09 21:48:27 +0000

    Create PeriodicTable.py

---
+++ PeriodicTable.py
@@ -0,0 +1,14 @@
+#only knows 4 elements sorry
+
+element = input("Enter element symbol: ").capitalize()
+
+if element == "H":
+    print("Hydrogen")
+elif element == "He":
+    print("Helium")
+elif element == "O":
+    print("Oxygen")
+elif element == "C":
+    print("Carbon")
+else:
+    print("Unknown element")
